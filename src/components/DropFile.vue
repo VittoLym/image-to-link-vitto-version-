8990aed5b5c7370a16f9022c3fe1af70cad51489
Vittossn <lexymdz@gmail.com>
2024-05-08 20:48:34 -0300

    create & rebuild components

---
+++ src/components/DropFile.vue
@@ -0,0 +1,127 @@
+<script setup>
+import { ref, defineEmits, } from 'vue'
+import { toast } from "vue3-toastify";
+import DropSvg from "./DropSvg.vue";
+const formElement = ref("");
+const emits = defineEmits(['urlImage','vtest','formElement'])
+const urlImage = ref('');
+const isDraging = ref(false);
+const vtest = ref(true)
+
+function handleInput(e) {
+  formElement.value = e.target.files[0];
+  emits('formElement', formElement.value)
+  if(formElement === undefined){
+    toast.error('try another type of image.',{
+      autoClose:3000,
+      closeButton:true,
+    })
+    vtest.value = true
+    emits('vtest', vtest.value)
+  }
+  else{
+    const reader = new FileReader();
+    reader.onload = function(e){
+      urlImage.value = e.target.result
+      emits('urlImage',urlImage)
+      vtest.value = true
+      emits('vtest', vtest.value)
+    }
+    reader.readAsDataURL(formElement.value)
+  }
+}
+
+function handleDrag(e) {
+  formElement.value = e.dataTransfer.files[0];
+  emits('formElement', formElement.value)
+  if(formElement.value === undefined){
+    toast.error('try another type of image.',{
+      autoClose:3000,
+      closeButton:true,
+    })
+    vtest.value = true
+    emits('vtest', vtest.value)
+  }
+  else{
+    const reader = new FileReader();
+    reader.onload = function(e){
+      urlImage.value = e.target.result
+      emits('urlImage', urlImage.value)
+      vtest.value = true
+      emits('vtest', vtest.value)
+    }
+    reader.readAsDataURL(formElement.value)
+  }
+    isDraging.value = false
+}
+
+function dragStyle() {
+  vtest.value = !vtest.value
+  isDraging.value= !isDraging.value
+  emits('vtest',vtest.value)
+}
+
+</script>
+<template>
+    <label
+      for="imagenInput"
+      class="archive-input"
+      :class="{'draging' : isDraging}"
+      @dragenter.prevent="dragStyle"
+      @dragleave.prevent="dragStyle"
+      @dragover.prevent
+      @drop.prevent="handleDrag"
+    >
+      Drop your Image:
+      <DropSvg v-if="!formElement?.name" :isDraging='isDraging'/>
+      <span v-else>{{formElement.name}}</span>
+    </label>
+    <input type="file" @change="handleInput" id="imagenInput" />
+</template>
+<style scoped>
+.drop {
+  margin: .8rem 0;
+  border-radius: 10px;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-name: moveDrop;
+}
+
+@keyframes moveDrop {
+  25% {
+    margin-top: 1.2rem;
+  }
+  75% {
+    margin-top: .4rem;
+  }
+}
+
+.archive-input {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50vw;
+  height: 20vh;
+  color: #fefefe;
+  background-color: #00000054;
+  border-radius: 10px;
+  transition: all ease .2s;
+  cursor: pointer;
+}
+
+.draging {
+  background-color: #00000099;
+}
+.draging-item {
+  fill: #00b52e;
+}
+input[type="file"] {
+  display: none;
+}
+
+span {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+</style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,16 +1,14 @@
 <script setup>
-import DropSvg from "./DropSvg.vue";
-import Links from "./Links.vue";
-import LoaderUI from "./LoaderUI.vue"
 import { ref } from "vue";
-import { toast } from "vue3-toastify";
+import Links from "./Links.vue";
+import LoaderUI from "./LoaderUI.vue";
+import DropFile from "./DropFile.vue";
+const IMG_DEFAULT = import.meta.env.VITE_DEFAULT_IMAGE;
+const urlImage = ref(IMG_DEFAULT)
+const vtest = ref(true)
+const formElement = ref('')
 const KEY = import.meta.env.VITE_API_SECRET;
 const ENDPOINT_CLOUD = import.meta.env.VITE_CLOUDINARY_URL;
-const IMG_DEFAULT = import.meta.env.VITE_DEFAULT_IMAGE;
-const formElement = ref("");
-const urlImage = ref(IMG_DEFAULT);
-const isDraging = ref(false);
-const vtest = ref(true)
 
 async function handleSubmit(e) {
   e.preventDefault();
@@ -34,44 +32,8 @@ async function handleSubmit(e) {
   }
 }
 
-function handleInput(e) {
-  formElement.value = e.target.files[0];
-  if(formElement === undefined){
-    toast.error('try another type of image.',{
-      autoClose:3000,
-      closeButton:true,
-    })
-  }
-  else{
-    const reader = new FileReader();
-    reader.onload = function(e){
-      urlImage.value = e.target.result
-    }
-    reader.readAsDataURL(formElement.value)
-  }
-}
-function handleDrag(e) {
-  formElement.value = e.dataTransfer.files[0];
-  if(formElement.value === undefined){
-    toast.error('try another type of image.',{
-      autoClose:3000,
-      closeButton:true,
-    })
-    vtest.value = true
-  }
-  else{
-    const reader = new FileReader();
-    reader.onload = function(e){
-      urlImage.value = e.target.result
-      vtest.value = true
-    }
-    reader.readAsDataURL(formElement.value)
-  }
-    isDraging.value = false
-}
-function dragStyle() {
-  vtest.value = !vtest.value
-  isDraging.value= !isDraging.value
+function eraseDrop(e){
+  console.log(e)
 }
 </script>
 <template>
@@ -82,20 +44,7 @@ function dragStyle() {
   />
   <LoaderUI v-else/>
   <form @submit="handleSubmit">
-    <label
-      for="imagenInput"
-      class="archive-input"
-      :class="{'draging' : isDraging}"
-      @dragenter.prevent="dragStyle"
-      @dragleave.prevent="dragStyle"
-      @dragover.prevent
-      @drop.prevent="handleDrag"
-    >
-      Drop your Image:
-      <DropSvg v-if="!formElement?.name" :isDraging='isDraging'/>
-      <span v-html="formElement?.name"></span>
-    </label>
-    <input type="file" @change="handleInput" id="imagenInput" />
+    <DropFile @urlImage=" urlImage = $event" @vtest="vtest = $event" @formElement="formElement = $event"/>
     <Links :urlImage="urlImage"/>
   </form>
 </template>
@@ -115,51 +64,11 @@ form {
   height: max-content;
   margin: 0;
 }
-.archive-input {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50vw;
-  height: 20vh;
-  color: #fefefe;
-  background-color: #00000054;
-  border-radius: 10px;
-  transition: all ease .2s;
-  cursor: pointer;
-}
-.draging {
-  background-color: #00000099;
-}
-.draging-item {
-  fill: #00b52e;
-}
-span {
-  font-size: 1.2rem;
-  font-weight: bold;
-}
-input[type="file"] {
-  display: none;
-}
 article {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   width: 50vw;
   margin: 1rem;
-}
-.drop {
-  margin: .8rem 0;
-  border-radius: 10px;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
-  animation-name: moveDrop;
-}
-@keyframes moveDrop {
-  25% {
-    margin-top: 1.2rem;
-  }
-  75% {
-    margin-top: .4rem;
-  }
 }
 </style>
